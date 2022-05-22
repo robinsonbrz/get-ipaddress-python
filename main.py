c656b36b18aab97793fb54116bb896c56162dc36
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-22 14:20:14 -0300

    funcao get ip

---
+++ main.py
@@ -0,0 +1,10 @@
+from http.server import BaseHTTPRequestHandler
+from pydoc import doc
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(self.headers.get('x-forwarded-for').encode())
+        return
