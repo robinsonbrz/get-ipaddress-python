e7e683d13597a151ef082c60d4e12f874e9899f3
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-23 15:40:10 -0300

    limpando comentarios

--- api/index.py
+++ api/index.py
@@ -2,8 +2,6 @@ from http.server import BaseHTTPRequestHandler
 from pydoc import doc
 
 
-# teste 01
-# teste 02
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
