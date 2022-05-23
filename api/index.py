3178a4646a10d76b50111877b624f99bf80b24ed
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-23 15:39:11 -0300

    teste 02

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
 from http.server import BaseHTTPRequestHandler
 from pydoc import doc
 
-# teste 01
 
+# teste 01
+# teste 02
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
